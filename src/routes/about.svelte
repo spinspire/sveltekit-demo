99c7779ba72b2a597b1a7322a75608abf0d1d812
Jitesh Doshi <jitesh@spinspire.com>
2022-07-18 21:03:07 -0400

    add "about" page

---
+++ src/routes/about.svelte
@@ -0,0 +1,11 @@
+<h1>About</h1>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium adipisci id aperiam minima,
+  inventore ipsam amet cumque. Iusto deleniti doloribus et tenetur iste suscipit dolorum autem
+  adipisci impedit ipsum optio aperiam nisi quia enim, incidunt culpa eligendi tempore, soluta eius
+  sint porro. Sint possimus praesentium nostrum accusantium sit assumenda blanditiis, consequatur
+  ipsum vero nulla fugiat voluptatem quo aliquam aliquid veniam laudantium. Optio id veritatis odit
+  iusto laudantium nulla doloremque dolorum magni laboriosam magnam fuga officia veniam, deleniti ad
+  cum itaque eius molestias facilis vel eos! Dolor officia laboriosam nulla, ipsum, optio quis
+  incidunt sequi laborum earum maiores praesentium. At, illo.
+</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>This project was created with <code>npm create svelte@next</code></p>
+<p>Link to <a href="about">about</a> page.</p>
