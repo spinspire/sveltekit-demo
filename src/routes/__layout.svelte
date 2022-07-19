2d55eaf8ad5ed50ffd0528b3498250e2699d4e33
Jitesh Doshi <jitesh@spinspire.com>
2022-07-18 20:58:10 -0400

    add basic __layout.svelte

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<main class="container">
+  <slot />
+</main>
+
+<style>
+  :global(body) {
+    background-color: var(--bs-light);
+    min-height: 100vh;
+  }
+  main {
+    background-color: var(--bs-white);
+  }
+</style>
