b75776fb2f56143b2bc36fb31e7f86a522bf85c1
Jitesh Doshi <jitesh@spinspire.com>
2022-07-19 00:02:46 -0400

    layout: semantic HTML with min-height

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,17 +2,29 @@
   import Nav from "$lib/components/Nav.svelte";
 </script>
 
-<Nav />
+<header>
+  <Nav />
+</header>
 <main class="container">
   <slot />
 </main>
+<footer>
+  <div class="bg-dark text-white">
+    <div class="container">Copyright &copy; 2022</div>
+  </div>
+</footer>
 
-<style>
+<style lang="scss">
   :global(body) {
     background-color: var(--bs-light);
-    min-height: 100vh;
+    > :global(div) {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+    }
   }
   main {
     background-color: var(--bs-white);
+    flex-grow: 1;
   }
 </style>
