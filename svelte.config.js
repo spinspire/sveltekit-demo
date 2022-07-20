ae5b48f972183730154a8d2e6872e4e87dae7ace
Jitesh Doshi <jitesh@spinspire.com>
2022-07-20 14:17:38 -0400

    add adpater-static/adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
-import adapter from "@sveltejs/adapter-auto";
+// import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
+// import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,6 +12,9 @@ const config = {
   kit: {
     adapter: adapter(),
     trailingSlash: "always",
+    prerender: {
+      // default: true,
+    },
   },
 };
 
