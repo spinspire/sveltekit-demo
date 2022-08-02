c42136c6505af39a701755be379924f31234cc50
Jitesh Doshi <jitesh@spinspire.com>
2022-08-02 14:19:45 -0400

    prevent inlining of assets in vite build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { isoImport } from "vite-plugin-iso-import";
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit(), isoImport()],
+  build: {
+    assetsInlineLimit: 0, // don't inline anything
+  }
 };
 
 export default config;
